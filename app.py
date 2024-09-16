d00f685e724c299d1106dc06c5b7b2aac62de507
Polo Sologub <hi@polosologub.com>
2024-09-16 22:05:19 +0100

    changed app run

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ api.add_resource(Index, '/')
 
 #Run app
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host='0.0.0.0')
+    # app.run(debug=True)
+    app.run(host='0.0.0.0')
 
 
 
