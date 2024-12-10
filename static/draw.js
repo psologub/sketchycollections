e14ab1f50fe7777c232067e8c6297771cc6dd6e5
Polo Sologub <hi@polosologub.com>
2024-12-10 03:49:35 -0800

    refactor

--- static/draw.js
+++ static/draw.js
@@ -1,25 +1,14 @@
-
 const canvas = document.getElementById("canvas");
 var canvas_wrapper = document.getElementById("canvas_wrapper");
 let context = canvas.getContext("2d");
-// canvas.style.width = 320 + "px";
-// canvas.style.height = 400 + "px";
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio
-// Set actual size in memory (scaled to account for extra pixel density).
 var scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
-// canvas.width = Math.floor(320 * scale);
-// canvas.height = Math.floor(400 * scale);
 var height = parseFloat(window.getComputedStyle(canvas, null)["height"].slice(0, -2))
 var width = parseFloat(window.getComputedStyle(canvas, null)["width"].slice(0, -2))
 canvas.width = Math.floor(width * scale);
 canvas.height = Math.floor(height * scale);
-
-// Normalize coordinate system to use css pixels.
 context.scale(scale, scale);
 
 let start_bg_color = "#f3f2f2";
-// let start_bg_color = "white";
 context.fillStyle = start_bg_color;
 context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -43,7 +32,7 @@ let index = -1;
 //for phone/tablet
 canvas.addEventListener("touchstart", start, false);
 canvas.addEventListener("touchmove", draw, false);
-//for computer
+//for desktop
 canvas.addEventListener("mousedown", start, false);
 canvas.addEventListener("mousemove", draw, false);
 
@@ -51,12 +40,9 @@ canvas.addEventListener("touchend", stop, false);
 canvas.addEventListener("mouseup", stop, false);
 canvas.addEventListener("mouseout", stop, false);
 
-//prepare drawing (event gets mouse coordinates)
 function start(event) {
     is_drawing = true;
-    //tells canvas to prepare to do something
     context.beginPath();
-    //get coordinates (what about canvas.offset?)
     if (event.type === 'mousedown') {
         context.moveTo(event.clientX - canvas.offsetLeft, 
             event.clientY - canvas.offsetTop);
@@ -64,15 +50,11 @@ function start(event) {
         context.moveTo(event.touches[0].clientX - canvas.offsetLeft, 
             event.touches[0].clientY - canvas.offsetTop);
     }
-    
-    //let default changes disappear (?)
-    event.preventDefault();
+        event.preventDefault();
 }
 
-//make drawing
 function draw(event) {
     if ( is_drawing ) {
-        //draw line to coordinates the mouse is moving to 
         if (event.type === 'mousemove') {
             context.lineTo(event.clientX - canvas.offsetLeft, 
                         event.clientY - canvas.offsetTop);
@@ -80,13 +62,10 @@ function draw(event) {
             context.lineTo(event.touches[0].clientX - canvas.offsetLeft, 
                 event.touches[0].clientY - canvas.offsetTop);
         }
-        //line styles
         context.strokeStyle = draw_color;
         context.lineWidth = draw_width;
         context.lineCap = "round";
-        //get an uninterrupted line
         context.lineJoin = "round";
-        //use the line styles
         context.stroke();
     }
     event.preventDefault();
@@ -100,9 +79,6 @@ function stop(event) {
     }
     event.preventDefault();
 
-    //when drawing stops, add paths into path array (push brings element to last position of an array)
-    //getImageData gets the image pixels of given rectangle
-    //will this work for phone?
     if ( event.type != "mouseout" ) {
     restore_array.push(context.getImageData(0, 0, canvas.width, canvas.height));
     index += 1;
@@ -126,9 +102,7 @@ function undo_last() {
         clear_canvas();
     } else {
         index -= 1;
-        //remove last element from array
         restore_array.pop();
-        //restore canvas with path data, start at top left corner (0,0)
         context.putImageData(restore_array[index], 0, 0);
     }
 }
--- static/predict.js
+++ static/predict.js
@@ -1,7 +1,3 @@
-function getData2(response) {
-  console.log(response['match_data'])      
-}
-
 function loadImage(url) {
     return new Promise((resolve, reject) => {
       const img = new Image();
@@ -9,7 +5,7 @@ function loadImage(url) {
       img.addEventListener('error', (err) => reject(err));
       img.src = url;
     });
-  }
+}
 
 function displayData(museum, image_url, object_link, title, targetTags, queryTags) {
   var iconSpan = document.createElement('span')
@@ -24,15 +20,8 @@ function displayData(museum, image_url, object_link, title, targetTags, queryTag
   if (document.getElementById(`${museum}_img_title`).childElementCount === 0) {
     document.getElementById(`${museum}_img_title`).appendChild(a);
   }
-  // document.getElementById(`${museum}_img_title`).appendChild(a);
-  // document.getElementById(`${museum}_img_title`).appendChild(iconSpan);
 
   $(`#${museum}_img_1`).css('background-image', 'url("' +  image_url + '")');
-  
-  // $(`#${museum}_img_title`).text(title);
-  // document.getElementById(`${museum}_img_title`).appendChild(iconSpan);
-  // $(`#${museum}_img_title`).text("Match #1");
-
   $(`#${museum}_query_title`).text("Your Image");
 
   for (let i = 0; i < 5; i++) {
@@ -41,183 +30,34 @@ function displayData(museum, image_url, object_link, title, targetTags, queryTag
     $(`div#${museum}_query_tag_body_${x}`).text(Object.keys(queryTags)[i]);
     $(`div#${museum}_query_tag_pred_${x}`).text(Object.values(queryTags)[i]);
     $(`div#${museum}_query_tag_pred_${x}`).css('text-align', 'right');
-
     $(`div#${museum}_match_tag_body_${x}`).text(Object.keys(targetTags)[i]);
     $(`div#${museum}_match_tag_pred_${x}`).text(Object.values(targetTags)[i]);
     $(`div#${museum}_match_tag_pred_${x}`).css('text-align', 'right');
   } 
 }
 
-
 function clearData(museum) {
   document.getElementById(`${museum}_error`).style.display = 'none';
 
   $(`#${museum}_img_1`).css('background-image', 'url("")');
-  
-  // $(`#${museum}_img_title`).text(title);
   $(`#${museum}_img_title`).text("");
-
-
   $(`#${museum}_query_title`).text("");
 
   for (let i = 0; i < 5; i++) {
     x = i + 1
     $(`div#${museum}_query_tag_body_${x}`).text("");
     $(`div#${museum}_query_tag_pred_${x}`).text("");
-
     $(`div#${museum}_match_tag_body_${x}`).text("");
     $(`div#${museum}_match_tag_pred_${x}`).text("");
   } 
 }
 
 function getDataCooper(data,csvUpload) {
-  var museum = 'smg';
-  clearData(museum);
-  document.getElementById("smg_lazy").style.display = 'grid';
-
-  if (csvUpload == true) {
-    var image_url = data['image_url']
-    var title = data['title']
-    var object_link = data['artwork_page']
-    var queryTags = data['query_tags'];
-    var objectTags = data['top_tags'];
-    var objectID = data['top_id'];
-
-    if (objectTags.length > 0) {
-      var targetTags = objectTags.shift();
-      var targetID = objectID.shift();
-      var targetTitle = title.shift()
-      var targetObjectLink = object_link.shift()
-      var targetImageUrl = image_url.shift()
-
-      var fallbackData = {
-        'image_url': data['image_url'].splice(0),
-        'title': data['title'].splice(0),
-        'query_tags': data['query_tags'],
-        'artwork_page': data['artwork_page'].splice(0),
-        'top_tags': data['top_tags'].slice(0),
-        'top_id': data['top_id'].slice(0)
-      }
-
-      return loadImage(targetImageUrl)
-      .then(function(img) {
-        console.log(img);
-        document.getElementById("smg_lazy").style.display = 'none';
-        displayData(museum, targetImageUrl, targetObjectLink, targetTitle, targetTags, queryTags);
-        })
-      .catch(function(err) {
-        console.log(err);
-        getDataTate(fallbackData);
-      })
-    } else {
-      document.getElementById("smg_lazy").style.display = 'none'
-      document.getElementById("smg_error").style.display = 'grid'; 
-    }
-  } else {
-    var image_url;
-    var title;
-    var object_link;
-    var queryTags = data['query_tags'];
-    var objectTags = data['top_tags'];
-    var objectID = data['top_id'];
-
-    if (objectTags.length > 0) {
-      var targetTags = objectTags.shift();
-      var targetID = objectID.shift();
-      var fallbackData = {
-        'query_tags': data['query_tags'],
-        'top_tags': data['top_tags'].slice(0),
-        'top_id': data['top_id'].slice(0)
-      }
-
-      var endpoint = "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&accession_number=";
-      var api_key = config.COOPER_API_KEY //TODO figure out env
-      url = endpoint + String(targetID) + api_key;
-
-      var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
-      
-      return fetch(url, requestOptions)
-      .then(response => response.json())
-      .then(function (response) {
-        image_url = response['object']['images'][0]['z']['url'];
-        title = response['object']['title']
-        object_link = response['object']['url']
-
-      return loadImage(image_url)
-      .then(function(img) {
-        console.log(img);
-        document.getElementById("smg_lazy").style.display = 'none';
-        displayData(museum, image_url, object_link, title, targetTags, queryTags);
-        })
-      .catch(function(err) {
-        console.log(err);
-        getDataCooper(fallbackData);
-      })
-      })
-      .catch(function(err) {
-        console.log(err);
-        getDataCooper(fallbackData);
-      })
-    } else {
-      document.getElementById("smg_lazy").style.display = 'none'
-      document.getElementById("smg_error").style.display = 'grid';
-    }
-  }
-}
-
-function getDataTate(data) {
-  var museum = 'met';
-  clearData(museum);
-  document.getElementById("met_lazy").style.display = 'grid';
-  var image_url = data['image_url']
-  var title = data['title']
-  var object_link = data['artwork_page']
-  var queryTags = data['query_tags'];
-  var objectTags = data['top_tags'];
-  var objectID = data['top_id'];
-
-  if (objectTags.length > 0) {
-    var targetTags = objectTags.shift();
-    var targetID = objectID.shift();
-    var targetTitle = title.shift()
-    var targetObjectLink = object_link.shift()
-    var targetImageUrl = image_url.shift()
-
-    var fallbackData = {
-      'image_url': data['image_url'].splice(0),
-      'title': data['title'].splice(0),
-      'query_tags': data['query_tags'],
-      'artwork_page': data['artwork_page'].splice(0),
-      'top_tags': data['top_tags'].slice(0),
-      'top_id': data['top_id'].slice(0)
-    }
-
-    return loadImage(targetImageUrl)
-    .then(function(img) {
-      console.log(img);
-      document.getElementById("met_lazy").style.display = 'none';
-      displayData(museum, targetImageUrl, targetObjectLink, targetTitle, targetTags, queryTags);
-      })
-    .catch(function(err) {
-      console.log(err);
-      getDataTate(fallbackData);
-    })
-  } else {
-    document.getElementById("met_lazy").style.display = 'none'
-    document.getElementById("met_error").style.display = 'grid'; //TODO update all tags to reflect museum
-  }
-}
-
-
-function getDataMet(data, csvUpload) {
   var museum = 'cooper';
   clearData(museum);
   document.getElementById("cooper_lazy").style.display = 'grid';
 
-  if (csvUpload === true) {
+  if (csvUpload == true) {
     var image_url = data['image_url']
     var title = data['title']
     var object_link = data['artwork_page']
@@ -254,6 +94,148 @@ function getDataMet(data, csvUpload) {
     } else {
       document.getElementById("cooper_lazy").style.display = 'none'
       document.getElementById("cooper_error").style.display = 'grid'; 
+    }
+  } else {
+    var image_url;
+    var title;
+    var object_link;
+    var queryTags = data['query_tags'];
+    var objectTags = data['top_tags'];
+    var objectID = data['top_id'];
+
+    if (objectTags.length > 0) {
+      var targetTags = objectTags.shift();
+      var targetID = objectID.shift();
+      var fallbackData = {
+        'query_tags': data['query_tags'],
+        'top_tags': data['top_tags'].slice(0),
+        'top_id': data['top_id'].slice(0)
+      }
+
+      var endpoint = "https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.objects.getInfo&accession_number=";
+      var api_key //insert API key here
+      url = endpoint + String(targetID) + api_key;
+
+      var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+      
+      return fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(function (response) {
+        image_url = response['object']['images'][0]['z']['url'];
+        title = response['object']['title']
+        object_link = response['object']['url']
+
+      return loadImage(image_url)
+      .then(function(img) {
+        console.log(img);
+        document.getElementById("cooper_lazy").style.display = 'none';
+        displayData(museum, image_url, object_link, title, targetTags, queryTags);
+        })
+      .catch(function(err) {
+        console.log(err);
+        getDataCooper(fallbackData);
+      })
+      })
+      .catch(function(err) {
+        console.log(err);
+        getDataCooper(fallbackData);
+      })
+    } else {
+      document.getElementById("cooper_lazy").style.display = 'none'
+      document.getElementById("cooper_error").style.display = 'grid';
+    }
+  }
+}
+
+function getDataTate(data) {
+  var museum = 'tate';
+  clearData(museum);
+  document.getElementById("tate_lazy").style.display = 'grid';
+  var image_url = data['image_url']
+  var title = data['title']
+  var object_link = data['artwork_page']
+  var queryTags = data['query_tags'];
+  var objectTags = data['top_tags'];
+  var objectID = data['top_id'];
+
+  if (objectTags.length > 0) {
+    var targetTags = objectTags.shift();
+    var targetID = objectID.shift();
+    var targetTitle = title.shift()
+    var targetObjectLink = object_link.shift()
+    var targetImageUrl = image_url.shift()
+
+    var fallbackData = {
+      'image_url': data['image_url'].splice(0),
+      'title': data['title'].splice(0),
+      'query_tags': data['query_tags'],
+      'artwork_page': data['artwork_page'].splice(0),
+      'top_tags': data['top_tags'].slice(0),
+      'top_id': data['top_id'].slice(0)
+    }
+
+    return loadImage(targetImageUrl)
+    .then(function(img) {
+      console.log(img);
+      document.getElementById("tate_lazy").style.display = 'none';
+      displayData(museum, targetImageUrl, targetObjectLink, targetTitle, targetTags, queryTags);
+      })
+    .catch(function(err) {
+      console.log(err);
+      getDataTate(fallbackData);
+    })
+  } else {
+    document.getElementById("tate_lazy").style.display = 'none'
+    document.getElementById("tate_error").style.display = 'grid'; 
+  }
+}
+
+
+function getDataMet(data, csvUpload) {
+  var museum = 'met';
+  clearData(museum);
+  document.getElementById("met_lazy").style.display = 'grid';
+
+  if (csvUpload === true) {
+    var image_url = data['image_url']
+    var title = data['title']
+    var object_link = data['artwork_page']
+    var queryTags = data['query_tags'];
+    var objectTags = data['top_tags'];
+    var objectID = data['top_id'];
+
+    if (objectTags.length > 0) {
+      var targetTags = objectTags.shift();
+      var targetID = objectID.shift();
+      var targetTitle = title.shift()
+      var targetObjectLink = object_link.shift()
+      var targetImageUrl = image_url.shift()
+
+      var fallbackData = {
+        'image_url': data['image_url'].splice(0),
+        'title': data['title'].splice(0),
+        'query_tags': data['query_tags'],
+        'artwork_page': data['artwork_page'].splice(0),
+        'top_tags': data['top_tags'].slice(0),
+        'top_id': data['top_id'].slice(0)
+      }
+
+      return loadImage(targetImageUrl)
+      .then(function(img) {
+        console.log(img);
+        document.getElementById("met_lazy").style.display = 'none';
+        displayData(museum, targetImageUrl, targetObjectLink, targetTitle, targetTags, queryTags);
+        })
+      .catch(function(err) {
+        console.log(err);
+        getDataTate(fallbackData);
+      })
+    } else {
+      document.getElementById("met_lazy").style.display = 'none'
+      document.getElementById("met_error").style.display = 'grid'; 
     }
   
   } else {
@@ -293,7 +275,7 @@ function getDataMet(data, csvUpload) {
       return loadImage(image_url)
       .then(function(img) {
         console.log(img);
-        document.getElementById("cooper_lazy").style.display = 'none';
+        document.getElementById("met_lazy").style.display = 'none';
         displayData(museum, image_url, object_link, title, targetTags, queryTags);
         })
       .catch(function(err) {
@@ -306,13 +288,13 @@ function getDataMet(data, csvUpload) {
         getDataMet(fallbackData)
       })
     } else {
-      document.getElementById("cooper_lazy").style.display = 'none'
-      document.getElementById("cooper_error").style.display = 'grid';
+      document.getElementById("met_lazy").style.display = 'none'
+      document.getElementById("met_error").style.display = 'grid';
     }
   }
 }
 
-
+// currently not in app – API only
 function getDataSMG(data) {
   var museum = 'smg';
   clearData(museum);
@@ -361,13 +343,11 @@ function getDataSMG(data) {
       })
     .catch(function(err) {
       console.log(err);
-      console.log('hi')
       getDataSMG(fallbackData)
     })
     })
     .catch(function(err) {
       console.log(err);
-      console.log('hi')
       getDataSMG(fallbackData)
     })
   } else {
@@ -377,7 +357,6 @@ function getDataSMG(data) {
 }
 
 $('#doodle_predict').click(function () {
-
   img = canvas.toDataURL();
 
   var myHeaders = new Headers();
@@ -395,7 +374,6 @@ $('#doodle_predict').click(function () {
   };
 
   fetch("/museum-retriever", requestOptions)
-  // fetch("http://localhost:8000/museum-retriever", requestOptions)
   .then(function (response) {
     if (response.ok) {
       return response.json();
@@ -403,9 +381,6 @@ $('#doodle_predict').click(function () {
       return Promise.reject(response);
     }
   }).then(async function (data) {
-    //TODO get all data here when env sorted
-    // var getDataAll = [getDataCooper(data['match_data']['cooper']), getDataMet(data['match_data']['met']), getDataSMG(data['match_data']['science'])] 
-    // var getDataAll = [getDataMet(data['match_data']['met']), getDataSMG(data['match_data']['science']), getDataTate(data['match_data']['tate'])]
     var getDataAll = [getDataMet(data['match_data']['met'],csvUpload=true), getDataCooper(data['match_data']['cooper'],csvUpload=true), getDataTate(data['match_data']['tate'])]
     return Promise.all(getDataAll)
   }
@@ -418,7 +393,7 @@ $('#doodle_predict').click(function () {
 
 
 $('#image_predict').click(function () {
-    var myHeaders = new Headers();
+  var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
   var raw = JSON.stringify({
@@ -432,7 +407,7 @@ $('#image_predict').click(function () {
     redirect: 'follow'
   };
 
-  fetch("http://127.0.0.1:5000/museum-retriever", requestOptions)
+  fetch("/museum-retriever", requestOptions)
   .then(function (response) {
     if (response.ok) {
       return response.json();
@@ -440,7 +415,7 @@ $('#image_predict').click(function () {
       return Promise.reject(response);
     }
   }).then(function (data) {
-    var getDataAll = [getDataCooper(data['match_data']['cooper']), getDataMet(data['match_data']['met']), getDataSMG(data['match_data']['science'])]
+    var getDataAll = [getDataMet(data['match_data']['met'],csvUpload=true), getDataCooper(data['match_data']['cooper'],csvUpload=true), getDataTate(data['match_data']['tate'])]
     return Promise.all(getDataAll)
   }
   ).then(function (data) {
